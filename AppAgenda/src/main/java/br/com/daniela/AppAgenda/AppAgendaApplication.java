e655b069bb1a6f352b53d8cf5025da4776104fad
daniela oliveira <danioliver_17@hotmail.com>
2023-10-28 18:31:22 -0300

    AppAgenda

---
+++ AppAgenda/src/main/java/br/com/daniela/AppAgenda/AppAgendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniela.AppAgenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAgendaApplication.class, args);
+	}
+
+}
---
+++ AppAgenda/src/test/java/br/com/daniela/AppAgenda/AppAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniela.AppAgenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
